2561ae15e9beae613c60dfd341fcb18240f58dc0
Loi Le <loile@Lois-MacBook-Pro.local>
2021-02-11 08:05:45 +0700

    fix: update tailwind config

--- workplace20-app/tailwind.config.js
+++ workplace20-app/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  purge: ['./pages/**/*.js', './components/**/*.js', './pages-lib/**/*.js', './layouts/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
 		colors: {
