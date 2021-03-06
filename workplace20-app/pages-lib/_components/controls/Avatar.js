65f7fad96f71ad4c9fd8c420a5f6e6be9d72326b
Loi Le <loile@Lois-MacBook-Pro.local>
2021-03-06 07:28:31 +0700

    fix: avatar issue

--- workplace20-app/pages-lib/_components/controls/Avatar.js
+++ workplace20-app/pages-lib/_components/controls/Avatar.js
@@ -15,7 +15,7 @@ const Avatar = ({
     return (<img className={classnames('mx-auto rounded-full', sizeStyles[size])} src={image} alt="avatar" />)
   } else {
     return (
-      <span className={classnames('inline-block rounded-full overflow-hidden bg-gray-100', `h-${size} w-${size}`)}>
+      <span className={classnames('inline-block rounded-full overflow-hidden bg-gray-100', sizeStyles[size])}>
         <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
           <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
