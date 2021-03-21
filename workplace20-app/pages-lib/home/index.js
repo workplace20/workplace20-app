f84023827a7dfe81b5cb6cd941b6345b225e9559
Paul K <paul.k@workplace20.com>
2021-03-21 20:53:44 +0700

    update home page content

--- workplace20-app/pages-lib/home/index.js
+++ workplace20-app/pages-lib/home/index.js
@@ -7,15 +7,14 @@ const Home = () => (
 				<div class="text-center">
 					<h1 class="text-2xl tracking-tight font-extrabold text-gray-600 sm:text-4xl md:text-4xl">
 						<span class="block xl:inline">
-							Remote jobs for developers in Viet Nam
+							Hire your next .NET & React.js developers in Viet
+							Nam
 						</span>
 						<span class="md:hidden"></span>
 					</h1>
 					<p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-						2020 has changed our workplace and working in the office
-						is optional. Workplace20 is a private community of
-						developers and software development business which offer
-						remote jobs in Viet Nam.
+						Workplace20 is a community of software engineers who
+						highly skilled in .NET, React.js
 					</p>
 				</div>
 			</div>
@@ -28,101 +27,56 @@ const Home = () => (
 							id="how-we-work"
 							class="text-3xl leading-8 font-extrabold tracking-tight text-orange-500 sm:text-4xl"
 						>
-							How do we work?
+							Founder's story
 						</h3>
+
 						<div class="flex flex-col md:flex-row">
-							<div class="pt-12 flex-6 pb-12">
-								<div class="flex">
-									<svg
-										class="flex-shrink-0 h-6 w-6 text-green-500"
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										aria-hidden="true"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
-											d="M5 13l4 4L19 7"
-										/>
-									</svg>
-									<div class="ml-3">
-										<dt class="text-lg leading-6 font-medium text-gray-900">
-											For software engineers
-										</dt>
-										<dd class="mt-2 text-base text-gray-500">
-											Workplace20 will work with you to
-											understand yourself and your skill
-											sets which help to match the team.
-											We offer the community sharing the
-											knowledge, best practices, and
-											experiences to do your best on the
-											remote job.
-										</dd>
-									</div>
-								</div>
-								<div class="flex pt-12">
-									<svg
-										class="flex-shrink-0 h-6 w-6 text-green-500"
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke="currentColor"
-										aria-hidden="true"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
-											d="M5 13l4 4L19 7"
-										/>
-									</svg>
-									<div class="ml-3">
-										<dt class="text-lg leading-6 font-medium text-gray-900">
-											For the business
-										</dt>
-										<dd class="mt-2 text-base text-gray-500">
-											Workplace20 helps to connect the
-											software engineers who match the
-											culture, skill sets, and interest in
-											your product domain.
-										</dd>
-									</div>
-								</div>
+							<div class="flex-1 pr-6 pt-12 flex-6 pb-12 leading-8 text-lg">
+								<p>
+									At the end of 2019, I have joined a FinTech
+									startup which it have no office in Viet Nam.
+									We had plan to build the office in Viet Nam
+									without know that the pandamic was coming.
+									Early of 2020, Viet Nam had started social
+									distance to prevent the spread of
+									coronavirus, there was no changes to come to
+									office to work.
+								</p>
+								<p class="pt-5">
+									Our journey for remote work began. We
+									started working remote for the whole team.
+									Beside that, we started to hire more
+									developers to join our team, of course,
+									completely remote. Our personal lifestyle
+									changed, we have started to burnout, lose
+									trust in team.
+								</p>
+								<p class="pt-5">
+									However, its not our end. We started to
+									learn how to build trust, avoid burnout and
+									transparent the work between team. Only hire
+									the developers from our network which match
+									our culture instead of ask the head hunting.
+									We have adopt agile manifesto, continues
+									ship our feature to production. We don't do
+									daily meeting but we always have online call
+									everyday. We don't have retrospective, but
+									we have lesson learn every problem we have.
+								</p>
+								<p class="pt-5">
+									By now, remote work make us freedom to work,
+									choose the place we want to live. Sai Gon is
+									too crownd. So why not build the community
+									to connect talent to work?
+								</p>
 							</div>
 
-							<div class="flex-6">
-								<div class="lg:m-0 lg:col-span-2 lg:pl-8">
-									<div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-10 lg:max-w-none">
-										<blockquote>
-											<div>
-												<svg
-													class="h-12 w-12 text-gray-600 opacity-25"
-													fill="currentColor"
-													viewBox="0 0 32 32"
-													aria-hidden="true"
-												>
-													<path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-												</svg>
-												<p class="mt-6 text-2xl font-medium text-gray-600">
-													Culture fit is a top
-													prerequisite when building
-													your remote development
-													team.
-												</p>
-											</div>
-											<footer class="mt-6">
-												<p class="text-base font-medium text-orange-600">
-													Paul K
-												</p>
-												<p class="text-base font-medium text-orange-300">
-													Creator of Workplace20
-												</p>
-											</footer>
-										</blockquote>
-									</div>
+							<div class="flex-1">
+								<div class="hidden md:inline-flex">
+									<img
+										class=" md:w-full md:h-1/2 md:h-1/2 md:object-cover"
+										src="https://images.unsplash.com/photo-1578768124932-e0cc82f7a37e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=666&q=80"
+									/>
 								</div>
 							</div>
 						</div>
@@ -221,60 +175,11 @@ const Home = () => (
 										office.
 									</dt>
 								</div>
-								{/*
-								<div class="bg-white">
-									<div class="max-w-7xl mx-auto sm:px-6 lg:py-8 lg:px-2">
-										<h2 class="inline text-3xl font-extrabold tracking-tight text-gray-700 sm:block sm:text-4xl">
-											Get in touch?
-										</h2>
-										<p class="inline text-3xl font-extrabold tracking-tight text-orange-400 sm:block sm:text-xl px-1">Give us your business email, we will get back to you.</p>
-										<form class="mt-8 sm:flex">
-											<label htmlFor="emailAddress" class="sr-only">Email address</label>
-											<input id="emailAddress" name="email" type="email" autoComplete="email" required class="w-full px-5 py-3 border-2 border-gray-200 placeholder-gray-500 focus:ring-orange-500 focus:border-orange-500 sm:max-w-xs rounded-md" placeholder="Enter your email"/>
-											<div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-												<button type="submit" class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
-													Submit
-												</button>
-											</div>
-										</form>
-									</div>
-								</div>
-								*/}
 							</dl>
 						</div>
 					</div>
 				</div>
 			</div>
-			{/*<div class="bg-white">
-				<div class="max-w-7xl mx-auto px-4 m:px-6 lg:px-8">
-					<div class="px-4 py-6 bg-orange-500 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center">
-						<div class="xl:w-0 xl:flex-1">
-							<h2 class="text-2xl font-extrabold tracking-tight text-white sm:text-3xl">
-								Want to learn more about news and updates from our community?
-							</h2>
-							<p class="mt-3 max-w-3xl text-lg leading-6 text-orange-200">
-								Sign up for our newsletter to stay up to date.
-							</p>
-						</div>
-						<div class="mt-8 sm:w-full sm:max-w-md xl:mt-0 xl:ml-8">
-							<form class="sm:flex">
-								<label htmlFor="emailAddress" class="sr-only">Email address</label>
-								<input id="emailAddress" name="emailAddress" type="email" autoComplete="email" required class="w-full px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-orange focus:ring-orange-600 rounded-md" placeholder="Enter your email"/>
-								<button type="submit" class="mt-3 w-full flex items-center justify-center px-5 py-3 border border-transparent shadow text-base font-medium rounded-md text-grey bg-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-orange-600 focus:ring-orange-600 sm:mt-0 sm:ml-3 sm:w-auto sm:flex-shrink-0">
-									Notify me
-								</button>
-							</form>
-							<p class="mt-3 text-sm text-orange-200">
-								We care about the protection of your data. Read our 
-								<a href="#" class="px-1 text-white font-medium underline">
-										Privacy Policy.
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			*/}
 			<div class="bg-white">
 				<div class="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
 					<h2 class="font-extrabold tracking-tight text-gray-700 ">
@@ -296,7 +201,6 @@ const Home = () => (
 			</div>
 		</div>
 	</div>
-
 );
 
 export default withMarketingLayout(Home);
