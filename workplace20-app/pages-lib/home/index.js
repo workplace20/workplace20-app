3aca47cabcf0907279fe4b3fdba3df38e4bcd0cd
Paul K <paul.k@workplace20.com>
2021-03-27 16:16:11 +0700

    update home page content

--- workplace20-app/pages-lib/home/index.js
+++ workplace20-app/pages-lib/home/index.js
@@ -3,71 +3,74 @@ import { withMarketingLayout } from "pages-lib/_layouts";
 const Home = () => (
 	<div className="bg-white">
 		<div>
-			<div class="mx-auto max-w-7xl px-4 pt-8 sm:pt-24">
-				<div class="text-center">
-					<h1 class="text-2xl tracking-tight font-extrabold text-gray-600 sm:text-4xl md:text-4xl">
+			<div class="flex mx-auto max-w-7xl px-4 pt-8 sm:pt-8 pb-16">
+				<div class="flex-initial text-center leading-16 w-1/2">
+					<h1 class="font-black text-orange-600 text-6xl pt-24">
 						<span class="block xl:inline">
-							Hire your next .NET & React.js developers in Viet
-							Nam
+							Hire .NET & React.js
 						</span>
-						<span class="md:hidden"></span>
 					</h1>
-					<p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-						Workplace20 is a community of software engineers who
-						highly skilled in .NET, React.js
+					<p class="bg-clip-text text-transparent bg-gradient-to-l from-orange-600 to-orange-400 sm:text-6xl">
+						remote developers in Viet Nam
 					</p>
+				</div>
+				<div class="flex-initial ml-24">
+					<img class="text-center" src="./assets/bg/vietnam.png" />
 				</div>
 			</div>
 		</div>
 		<div class="relative overflow-hidden">
 			<div class="relative ">
 				<div class="relative pt-12 pb-16 px-4 ">
-					<div class="text-base max-w-4xl mx-auto ">
-						<h3
+					<div class="text-base max-w-6xl mx-auto ">
+						<h2
 							id="how-we-work"
 							class="text-3xl leading-8 font-extrabold tracking-tight text-orange-500 sm:text-4xl"
 						>
 							Founder's story
-						</h3>
+						</h2>
 
 						<div class="flex flex-col md:flex-row">
-							<div class="flex-1 pr-6 pt-12 flex-6 pb-12 leading-8 text-lg">
+							<div class="flex-1 pr-6 pt-12 flex-6 pb-12 leading-8 text-xl">
 								<p>
 									At the end of 2019, I have joined a FinTech
-									startup which it have no office in Viet Nam.
-									We had plan to build the office in Viet Nam
-									without know that the pandamic was coming.
-									Early of 2020, Viet Nam had started social
+									startup that has no office in Viet Nam. We
+									had the plan to build the office in Viet Nam
+									without know that the pandemic was coming.
+									Early in 2020, Viet Nam had started social
 									distance to prevent the spread of
-									coronavirus, there was no changes to come to
-									office to work.
+									coronavirus, there were no changes to come
+									to the office to work.
 								</p>
 								<p class="pt-5">
 									Our journey for remote work began. We
-									started working remote for the whole team.
-									Beside that, we started to hire more
+									started working remotely for the whole team.
+									Besides that, we started to hire more
 									developers to join our team, of course,
 									completely remote. Our personal lifestyle
-									changed, we have started to burnout, lose
-									trust in team.
+									changed, we have started to burnout,
+									stressed.
 								</p>
 								<p class="pt-5">
-									However, its not our end. We started to
+									However, it's not our end. We started to
 									learn how to build trust, avoid burnout and
-									transparent the work between team. Only hire
-									the developers from our network which match
-									our culture instead of ask the head hunting.
-									We have adopt agile manifesto, continues
-									ship our feature to production. We don't do
-									daily meeting but we always have online call
-									everyday. We don't have retrospective, but
-									we have lesson learn every problem we have.
+									transparent about the work between the team.
+									Only hire the developers from our network
+									who match our culture instead of asking the
+									headhunting. We have adopted the agile
+									manifesto, continues to ship our feature to
+									production. We don't do daily meeting but we
+									always have an online call every day. We
+									don't have a retrospective meeting end of
+									the sprint, but we have a lesson-learn for
+									every problem we have.
 								</p>
 								<p class="pt-5">
-									By now, remote work make us freedom to work,
-									choose the place we want to live. Sai Gon is
-									too crownd. So why not build the community
-									to connect talent to work?
+									By now, remote work helps us the freedom to
+									work, choose the place we want to live. Sai
+									Gon is too crowd. So why not build the
+									community to connect talent to work
+									remotely?
 								</p>
 							</div>
 
@@ -84,98 +87,109 @@ const Home = () => (
 					</div>
 				</div>
 			</div>
-
 			<div class="relative bg-white">
 				<div class="relative px-4 ">
-					<div class="text-base max-w-4xl mx-auto ">
-						<h3 class="text-3xl leading-8 font-extrabold tracking-tight text-orange-500 sm:text-4xl">
+					<div class="ax-w-4xl mx-auto ">
+						<h2 class="font-black sm:text-8xl bg-clip-text text-transparent bg-gradient-to-l from-orange-400 to-orange-600">
 							Build by developers, for developers
-						</h3>
+						</h2>
+						<p class="py-6 font-black sm:text-4xl bg-clip-text text-transparent bg-gradient-to-l from-orange-300 to-orange-600 sm:text-4xl">
+							The challenges when looking for a remote job
+						</p>
 					</div>
 				</div>
-				<div class="max-w-4xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-					<h2
-						id="for-engineer"
-						class="text-3xl font-extrabold text-gray-600"
-					>
-						For Sofware Engineers
-					</h2>
-					<div class="mt-2 border-t border-opacity-25 pt-10 text-gray-500">
-						<dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-1 md:gap-x-4 md:gap-y-6">
-							<div>
-								<dt class="text-lg leading-6 font-medium text-gray">
-									✨ As you're working remotely, you can work
-									anywhere. Balance your lifestyle and
-									freedom.
-								</dt>
+				<div class="mt-16">
+					<div class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+						<div class="flex">
+							<div class="flex-initial max-w-6xl">
+								<h2 class="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+									No Trust.
+								</h2>
+								<p class="mt-6 max-w-3xl text-xl leading-8 text-gray-700">
+									As remote work is only work in a trusted
+									environment, People don't know who you are,
+									they need you to prove that you can do the
+									job, this leads to lots of interview
+									session. And when they hire you, they're
+									making a bet on you.
+								</p>
 							</div>
-							<div>
-								<dt class="text-lg font-medium text-gray">
-									✨ A growth of business community which
-									provides the remote work, flexible of
-									culture and innovative products.
-								</dt>
+							<div class="flex-initial">
+								<img src="./assets/bg/trust.png" />
 							</div>
-							<div>
-								<dt class="text-lg font-medium text-grey">
-									✨ The community of like-minded people who
-									can support and share experience on remote
-									work, self-discipline, and motivation.
-								</dt>
-							</div>
-							<div class="pt-12 mt-6 relative">
-								<a
-									href="#"
-									class="absolute bottom-0 right-0 inline-flex px-4 py-2 border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-orange-600 to-orange-400 hover:from-orange-700 hover:to-orange-500"
-								>
-									A Day in the Life of a Remote Developer
-								</a>
-							</div>
-						</dl>
+						</div>
 					</div>
-					<div class="max-w-4xl mx-auto py-12">
-						<h2
-							id="for-business"
-							class="text-3xl font-extrabold text-gray-600"
-						>
-							For Business
+					<div class="mt-16 mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+						<div class="flex">
+							<div class="flex-initial">
+								<h2 class="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+									Culture fit.
+								</h2>
+								<p class="mt-6 max-w-3xl text-xl leading-8 text-gray-700">
+									Look at this
+									<a
+										class="pl-1 pr-1 text-orange-600"
+										href="https://neilonsoftware.com/difficult-people-on-software-projects/"
+									>
+										https://neilonsoftware.com/difficult-people-on-software-projects.
+									</a>
+									It's not about your coding skills at all.
+									People want to work with engineers who
+									like-mind, so together, they can collaborate
+									to ship the software product.
+								</p>
+							</div>
+							<div class="flex-initial">
+								<img src="./assets/bg/culture.png" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="relative pt-32 bg-white">
+				<div class="relative px-4 ">
+					<div class="ax-w-4xl mx-auto ">
+						<h2 class="font-black sm:text-8xl bg-clip-text text-transparent bg-gradient-to-l from-orange-400 to-orange-600">
+							Join Us,
 						</h2>
-						<div class="mt-2 border-t border-opacity-25 pt-10 text-gray-500">
-							<dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-1 md:gap-x-4 md:gap-y-6">
-								<div>
-									<dt class="text-lg font-medium text-gray">
-										✨ The talented engineers from anyplace.
-									</dt>
-								</div>
-								<div>
-									<dt class="text-lg font-medium text-grey">
-										✨ A selection of software engineers
-										built on trust.
-									</dt>
-								</div>
-
-								<div>
-									<dt class="text-lg leading-6 font-medium text-gray">
-										✨ The community of software engineers
-										who are ambitious and grit. They have
-										experienced in remote work,
-										self-discipline, high skill, and
-										commitment.
-									</dt>
-								</div>
-
-								<div>
-									<dt class="text-lg leading-6 font-medium text-gray">
-										✨ Transparent as you pay them directly.
-									</dt>
-								</div>
-								<div>
-									<dt class="text-lg leading-6 font-medium text-gray">
-										✨ Reduce the cost of trust as no
-										office.
-									</dt>
-								</div>
-							</dl>
+						<p class="py-6 font-black sm:text-4xl bg-clip-text text-transparent bg-gradient-to-l from-orange-600 to-orange-300 sm:text-4xl">
+							Be the part of .NET and React.js Developer in Viet
+							Nam
+						</p>
+					</div>
+				</div>
+				<div class="mt-16">
+					<div class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+						<div class="flex">
+							<div class="flex-initial max-w-6xl pr-20">
+								<h2 class="text-3xl font-extrabold text-gray-700 tracking-tight sm:text-4xl">
+									1. Register an account and take our
+									challenges
+								</h2>
+								<p class="mt-6 max-w-3xl text-xl leading-8 text-gray-600">
+									We designed our challenge to understand your
+									skill and hands-on experience by using the
+									real challenge as a software engineer like
+									design a feature, doing code review,
+									refactoring code, write test. We don't ask
+									you the theory questions.
+								</p>
+							</div>
+							<div class="flex-initial">
+								<h2 class="text-3xl font-extrabold text-gray-700 tracking-tight sm:text-4xl">
+									2. Learn, share and ready for your next
+									remote job.
+								</h2>
+								<p class="mt-6 max-w-3xl text-xl leading-8 text-gray-700">
+									We build a high-quality community in .NET
+									and React.js with deep-dive technical docs,
+									you can learn from them as well as share
+									your writeup to the community, get
+									reputation and trust. Be ready and connect
+									with people for your next remote job.
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -183,17 +197,15 @@ const Home = () => (
 			<div class="bg-white">
 				<div class="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
 					<h2 class="font-extrabold tracking-tight text-gray-700 ">
-						<span class="block text-3xl sm:text-4xl">
-							Want to build a remote development team?
-						</span>
+						<span class="block text-4xl sm:text-4xl">Ready?</span>
 					</h2>
 					<div class="mt-8 flex justify-center">
 						<div class="inline-flex rounded-md shadow">
 							<a
-								href="https://linkedin.com/company/workplace20"
+								href="/api/auth/signin"
 								class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700"
 							>
-								Connect with us on LinkedIn
+								Click to Sign Up
 							</a>
 						</div>
 					</div>
