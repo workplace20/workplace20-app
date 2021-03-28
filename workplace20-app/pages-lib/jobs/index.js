eccc6c667d278acdd8aec0e2bc08e68dccb815f7
Loi Le <loile@Lois-MacBook-Pro.local>
2021-03-28 21:15:44 +0700

    feat: update jobs static pages

--- workplace20-app/pages-lib/jobs/index.js
+++ workplace20-app/pages-lib/jobs/index.js
@@ -2,9 +2,23 @@ import { withMarketingLayout } from 'pages-lib/_layouts';
 import { JobList } from './_components';
 
 const JobListPage = () => {
+  const jobs = Array(9)
+  .fill(0)
+  .map((_, i) => ({
+    id: i,
+    title: 'Front-end Developer',
+    companyName: 'The Boring Company',
+    level: 'Level',
+    english: 'Advance',
+    introduction: 'libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit amet aliquam id diam maecenas ultricies mi',
+    skills: ["react", "csharp"],
+    url: `/jobs/${i}`
+  }));
+
   return (
     <div className="my-8 px-6">
       <JobList 
+        jobs={jobs}
       />
     </div>
   )
--- workplace20-app/pages-lib/jobs/me/index.js
+++ workplace20-app/pages-lib/jobs/me/index.js
@@ -3,10 +3,24 @@ import { JobList } from '../_components';
 import Card from 'pages-lib/_components/card';
 
 const JobListPage = () => {
+  const jobs = Array(9)
+    .fill(0)
+    .map((_, i) => ({
+      id: i,
+      title: 'Front-end Developer',
+      companyName: 'The Boring Company',
+      level: 'Level',
+      english: 'Advance',
+      introduction: 'libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit amet aliquam id diam maecenas ultricies mi',
+      skills: ["react", "csharp"],
+      url: `/jobs/me/${i}`
+    }));
+
   return (
     <Card>
       <Card.Content>
-        <JobList 
+        <JobList
+          jobs={jobs}
           cardType="box"
         />
       </Card.Content>
